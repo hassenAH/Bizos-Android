eaad3269cba7d096ed261687ef7808cd89d810cd
wissalChebb <98927814+wissalChebb@users.noreply.github.com>
2023-04-06 23:19:46 +0100

    fix retrofit

--- app/src/main/java/com/instamobile/kotlinlogin/Retrofit/IServices.kt
+++ app/src/main/java/com/instamobile/kotlinlogin/Retrofit/IServices.kt
@@ -5,14 +5,14 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface IServices {
-    @POST("compte")
+    @POST("user/compte")
     @FormUrlEncoded
     fun registerUser(@Field("first_name") firstName:String,
                      @Field("last_name") lastName:String,
                      @Field("email") email:String,
                      @Field("password") password:String
                      )
-    @POST("Signin")
+    @POST("user/Signin")
     @FormUrlEncoded
     fun loginUser( @Field("email") email:String,
                    @Field("password") password:String)
---
+++ app/src/main/java/com/instamobile/kotlinlogin/Retrofit/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.instamobile.kotlinlogin.Retrofit
+
+import com.facebook.CallbackManager
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+object RetrofitClient {
+    private var instance: Retrofit?=null
+    fun  getInstance(): Retrofit {
+        if (instance == null)
+            instance=Retrofit.Builder().baseUrl("http://localhost:5000/")
+                .addConverterFactory( ScalarsConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
+        return instance!!
+    }
+}
