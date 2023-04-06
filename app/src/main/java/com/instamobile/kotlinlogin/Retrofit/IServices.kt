caac6ccaecd47ef4941cf9e1e996569fd7243848
wissalChebb <98927814+wissalChebb@users.noreply.github.com>
2023-04-06 22:55:33 +0100

    retrofit configuration

---
+++ app/src/main/java/com/instamobile/kotlinlogin/Retrofit/IServices.kt
@@ -0,0 +1,19 @@
+package com.instamobile.kotlinlogin.Retrofit
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface IServices {
+    @POST("compte")
+    @FormUrlEncoded
+    fun registerUser(@Field("first_name") firstName:String,
+                     @Field("last_name") lastName:String,
+                     @Field("email") email:String,
+                     @Field("password") password:String
+                     )
+    @POST("Signin")
+    @FormUrlEncoded
+    fun loginUser( @Field("email") email:String,
+                   @Field("password") password:String)
+}
