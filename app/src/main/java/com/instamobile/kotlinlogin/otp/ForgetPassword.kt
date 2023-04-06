2bc8860ec815ab4d0223f3384be395185ef5d090
hassen <hassen.ahmadi@esprit.tn>
2023-04-07 01:02:40 +0200

    some fixes

--- app/src/main/java/com/instamobile/kotlinlogin/otp/ForgetPassword.kt
+++ app/src/main/java/com/instamobile/kotlinlogin/otp/ForgetPassword.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import com.instamobile.kotlinlogin.MainActivity
 import com.instamobile.kotlinlogin.R
@@ -14,7 +15,7 @@ class ForgetPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
         val txtEmail = findViewById<EditText>(R.id.txtEmail)
-        val btn_send = findViewById<EditText>(R.id.btn_send)
+         var btn_send = findViewById<Button>(R.id.btn_send);
         btn_send.setOnClickListener()
         {
             val intent = Intent(this@ForgetPassword, Otp::class.java)
