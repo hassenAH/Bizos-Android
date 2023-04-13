c06e4105596f544736a3858639d3a47a26388544
hassen <hassen.ahmadi@esprit.tn>
2023-04-14 01:27:38 +0200

    lawyer home and side bar

---
+++ app/src/main/java/com/instamobile/kotlinlogin/LawyerHome.kt
@@ -0,0 +1,11 @@
+package com.instamobile.kotlinlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LawyerHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lawyer_home)
+    }
+}
